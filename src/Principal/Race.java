95c931bc755a6e8cf071c14f2670270f80617508
EmannuelBezerra <emannuelbezerra678@gmail.com>
2025-10-07 10:39:18 -0300

    Adição da prioridade

--- src/Principal/Race.java
+++ src/Principal/Race.java
@@ -12,9 +12,21 @@ public class Race {
 		for(int i = 1; i <= 10; i++) {
 			RacerRunnable racer = new RacerRunnable(i);
 			Thread thread = new Thread(racer);
+			
+			//Definindo prioridades
+			if(i <= 3) {
+				thread.setPriority(Thread.MIN_PRIORITY);
+			}else if (i <= 7) {
+				thread.setPriority(Thread.NORM_PRIORITY);
+			}else {
+				thread.setPriority(Thread.MAX_PRIORITY);
+			}
+			
+			
 			racers.add(thread);
 		}
 		
+		System.out.println("Prioridade minima: Racers 1,2,3\nPrioridade normal: Racers 4,5,6,7\nPrioridade máxima: racers 8,9,10");
 		for(Thread a : racers) {
 			a.start();
 		}
