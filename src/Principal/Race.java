6e7f95de6e4f7ba6d4da34b53769b3303833215f
EmannuelBezerra <emannuelbezerra678@gmail.com>
2025-10-11 15:23:54 -0300

    Configuração da corrida e join

--- src/Principal/Race.java
+++ src/Principal/Race.java
@@ -8,27 +8,51 @@ import Racers.RacerRunnable;
 public class Race {
 
 	public static void main(String[] args) {
-		List<Thread> racers = new ArrayList<>();
+		List<Thread> racersPares = new ArrayList<>();
+		List<Thread> racersImpares = new ArrayList<>();
 		for(int i = 1; i <= 10; i++) {
 			RacerRunnable racer = new RacerRunnable(i);
 			Thread thread = new Thread(racer);
 			
-			//Definindo prioridades
-			if(i <= 3) {
-				thread.setPriority(Thread.MIN_PRIORITY);
-			}else if (i <= 7) {
-				thread.setPriority(Thread.NORM_PRIORITY);
+			if(i%2==0) {
+				racersPares.add(thread);
 			}else {
-				thread.setPriority(Thread.MAX_PRIORITY);
+				racersImpares.add(thread);
 			}
-			
-			
-			racers.add(thread);
 		}
 		
-		
-		for(Thread a : racers) {
+		//Iniciando ímpares
+		System.out.println("Iniciando impares");
+		for(Thread a : racersImpares) {
 			a.start();
 		}
+		
+		try {
+			System.out.println("Esperando impares terminarem...");
+			for(Thread a : racersImpares) {
+				a.join();
+			}
+			System.out.println("Impares terminaram.");
+		}catch (InterruptedException e){
+			e.printStackTrace();
+		}
+		
+		//Iniciando os pares
+		System.out.println("Iniciando pares");
+		for(Thread a : racersPares) {
+			a.start();
+		}
+		
+		try {
+			System.out.println("Esperando pares terminarem...");
+			for(Thread a : racersPares) {
+				a.join();
+			}
+			System.out.println("Pares terminaram.");
+		}catch (InterruptedException e){
+			e.printStackTrace();
+		}
+		
+		System.out.println("===FIM===");
 	}
 }
--- src/Racers/RacerRunnable.java
+++ src/Racers/RacerRunnable.java
@@ -2,6 +2,7 @@ package Racers;
 
 public class RacerRunnable implements Runnable{
 	private int i;
+	private static final int MAX_PRINTS = 10;
 	
 	public RacerRunnable(int i) {
 		this.i = i;
@@ -9,14 +10,15 @@ public class RacerRunnable implements Runnable{
 	
 	@Override
 	public void run() {
-		while(true) {
-			System.out.println("Racer Runnable "+i+" imprimindo");
+		for(int count = 1; count <= MAX_PRINTS; count++) {
+			System.out.println("Racer Runnable "+i+" imprimindo "+count+"/"+MAX_PRINTS);
 			
 			try {
-				Thread.sleep(10);
+				Thread.sleep(1);
 			}catch(InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
+		System.out.println("Racer "+i+" FINALIZOU");
 	}
 }
